811933cd56126733f1e377814e18b238468691f4
Cristian <calvarado@bow.cl>
2015-02-05 11:27:28 -0300

    base template

--- wp-config.php
+++ wp-config.php
@@ -71,8 +71,13 @@ $table_prefix  = 'cp_';
 define('WP_DEBUG', false);
 
 //WP-CONTENT outside core
-define( 'WP_CONTENT_DIR', dirname (dirname(__FILE__)) . '/wp-content' );
-define( 'WP_CONTENT_URL', '/wp-content' );
+// define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
+// define( 'WP_CONTENT_URL', 'http://dev.local.bowl.cl/labs/cp/wp/wp-content' );
+
+if ( !defined('WP_CONTENT_DIR') )
+	define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
+// if ( !defined('WP_CONTENT_URL') )
+	// define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
 
 /* That's all, stop editing! Happy blogging. */
 
@@ -82,3 +87,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+if ( !defined('WP_CONTENT_URL') )
+	define( 'WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
