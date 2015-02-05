c602984116b8bde2888ee49b8ef2f632c1b7252c
Cristian <calvarado@bow.cl>
2015-02-05 17:35:20 -0300

    update sub page

---
+++ wp-content/themes/cp/functions.php
@@ -0,0 +1,28 @@
+<?php
+/*
+*  Create an advanced sub page
+*/
+
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'Home',
+	// 	'menu_title'	=> 'Home',
+	// 	'parent_slug'	=> 'theme-general-settings',
+	// ));
+
+	// acf_add_options_sub_page(array(
+	// 	'page_title' 	=> 'SEO',
+	// 	'menu_title'	=> 'SEO',
+	// 	'parent_slug'	=> 'theme-general-settings',
+	// ));
+
+}
