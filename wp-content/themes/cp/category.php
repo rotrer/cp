c637dbc566cf8c173c49037b05e55046dbbb411b
rotrer <cristian@rotrer.com>
2015-11-06 00:00:18 -0300

    update

--- wp-content/themes/cp/category.php
+++ wp-content/themes/cp/category.php
@@ -7,7 +7,7 @@ get_header();
   $thisCat = get_category(get_query_var('cat'));
 	if ( in_category( array(2,3,4,5,6) ) ) {
 		include TEMPLATEPATH . '/templates/category-blog.php';
-	} elseif ( $thisCat->parent == 36 ) {
+	} elseif ( $thisCat->term_id == 36 || $thisCat->parent == 36 ) {
     include TEMPLATEPATH . '/templates/category-galerias.php';
   }
 get_footer();
