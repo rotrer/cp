200951057dc5869894a8b3071753141253118d60
Cristian A <rotrer@gmail.com>
2016-02-02 10:09:35 -0300

    imagen galeria

--- wp-content/themes/cp/templates/category-galerias.php
+++ wp-content/themes/cp/templates/category-galerias.php
@@ -17,13 +17,13 @@
         ) );
         if ( $categories ) foreach ($categories as $key => $category) {
         ?>
-        <?php $photo = get_field('imagen_cat', $category); ?>
+        <?php $photo = get_field('imagen_cat2', $category); ?>
 
 
 
         <div class="col-md-4 " data-w="<?php echo $photo['width'] ?>" data-h="<?php echo $photo['height'] ?>">
           <div class="archive-cat">
-          <a href="<?php echo get_category_link( $category->term_id ); ?>"><img  src="<?php echo $photo; ?>"> 
+          <a href="<?php echo get_category_link( $category->term_id ); ?>"><img  src="<?php echo $photo['url']; ?>"> 
 
             <!-- descripcion imagen --> 
            <div class="description"><h3><?php echo $category->name; ?></h3></div></a>
