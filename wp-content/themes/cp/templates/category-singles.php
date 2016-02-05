976edccd1d46a73387d5b6e3466e877a4102fe71
Cristian A <rotrer@gmail.com>
2016-02-05 17:55:59 -0300

    csm

--- wp-content/themes/cp/templates/category-singles.php
+++ wp-content/themes/cp/templates/category-singles.php
@@ -21,16 +21,9 @@
         query_posts( array('cat' => $thisCat->term_id, 'post_type' => 'archivos') ); 
         if ( have_posts() ) while ( have_posts() ) : the_post();
           $imagen_destacada = get_field('imagen_destacada');
-          global $is_mobile;
-          if ($is_mobile) {
-            $photo_featured = $imagen_destacada["sizes"]["galeria-thumbx"];
-            $photo_width = $imagen_destacada["sizes"]["galeria-thumbx-width"];
-            $photo_height = $imagen_destacada["sizes"]["galeria-thumbx-height"];
-          } else {
-            $photo_featured = $imagen_destacada["url"];
-            $photo_width = $imagen_destacada["width"];
-            $photo_height = $imagen_destacada["height"];
-          }
+          $photo_featured = $imagen_destacada["sizes"]["galeria-thumbx"];
+          $photo_width = $imagen_destacada["sizes"]["galeria-thumbx-width"];
+          $photo_height = $imagen_destacada["sizes"]["galeria-thumbx-height"];
         ?>
         <div class="item col-md-4" data-w="<?php echo $photo_width; ?>" data-h="<?php echo $photo_height; ?>">
           <div class="archive-cat">
