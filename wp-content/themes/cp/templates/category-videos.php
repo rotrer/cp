10604f633f877ee28d7144eb1856a1e8d6cf0c70
leo <leonrov@gmail.com>
2016-02-19 11:31:20 -0300

    Grilla y pagina video

---
+++ wp-content/themes/cp/templates/category-videos.php
@@ -0,0 +1,43 @@
+<?php 
+  $thisCat = get_category(get_query_var('cat')); 
+  $id_singles = '';
+?>
+<?php if ($thisCat->term_id == 47) { $id_singles = 'flex'; ?>
+<script>
+  $(document).ready(function(){
+    $('#flex').flexImages();
+  });
+</script>
+<?php }elseif ($thisCat->term_id == 39) { $id_singles = 'portadas'; ?>
+<?php } ?>
+<section class="container">
+    <div class="row">
+      <header class="title">
+         
+        <h1><?php echo $thisCat->name; ?></h1>
+        <h3> Archivos</h3>
+   
+      </header>
+      <div id="<?php echo $id_singles; ?>">
+        <?php 
+        query_posts( array('cat' => $thisCat->term_id, 'post_type' => 'archivos') ); 
+        if ( have_posts() ) while ( have_posts() ) : the_post();
+          $imagen_destacada = get_field('imagen_destacada');
+          $photo_featured = $imagen_destacada["sizes"]["galeria-thumbx"];
+          $photo_width = $imagen_destacada["sizes"]["galeria-thumbx-width"];
+          $photo_height = $imagen_destacada["sizes"]["galeria-thumbx-height"];
+        ?>
+        <div class="item col-xs-6 col-sm-6 col-md-6 " data-w="<?php echo $photo_width; ?>" data-h="<?php echo $photo_height; ?>">
+          <div class="archive-cat">
+            <a href="<?php the_permalink(); ?>"><img  src="<?php echo $photo_featured; ?>"> 
+              <!-- descripcion imagen -->
+              <div class="description">
+                <h2><?php the_title(); ?></h2>
+              </div>
+            </a>
+          </div>
+        </div>
+        <?php endwhile; wp_reset_query(); ?>
+      </div>
+    </div>
+</section>
