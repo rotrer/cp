54308e336c052b9ec33b976d757c242f73a932b9
Leonardo Rodriguez <leonrov@leonrov.local>
2016-04-03 10:34:49 -0300

    contacto

--- wp-content/themes/cp/templates/page-contacto.php
+++ wp-content/themes/cp/templates/page-contacto.php
@@ -17,16 +17,27 @@
 			<?php endwhile; ?>
 		</div>
 		<div class="col-md-5 contact-info">
-			<h3 class="psm">PARSONS FASHION MANAGEMENT</h3>
-			<p><span>Santiago de Chile </span>
+			<a href="http://www.parsons.cl/web/">
+				<img class="contact-logo" src="<?php bloginfo( 'template_directory' ); ?>/images/pfm.jpg" alt="PARSONS FASHION MANAGEMENT">
+			</a>
+			<a href="http://www.parsons.cl/web/">www.parsons.cl</a>
+			<p><span>Santiago de Chile </span>	
 			<span>T: 562 – 29515214</span></p>
-			<h3 class=" mc2">MC2</h3>
+
+			<a href="http://www.parsons.cl/web/">
+				<img class="contact-logo" src="<?php bloginfo( 'template_directory' ); ?>/images/mc2.jpg" alt="MC2">
+			</a>
+			<a href="http://www.mc2models.com/">www.mc2models.com</a>
 			<p>
 			<span>6 West 14 TH St. 2nd Floor</span>
 			<span>NY, NY 10011</span>
 			<span>T: 646 – 638 – 3330</span>
 			<span>F: 646 – 638 – 2123</span></p>
-			<h3 class="ming">MING MANAGEMENT</h3>
+
+			<a href="http://www.parsons.cl/web/">
+				<img class="contact-logo" src="<?php bloginfo( 'template_directory' ); ?>/images/ming.jpg" alt="MING MANAGEMENT">
+			</a>
+			<a href="http://www.modelmanagement.com/">www.modelmanagement.com</a>
 			<p>
 			<span>Sao Paulo, Brazil</span>
 			<span>T: 55 11 5083 9539</span>
