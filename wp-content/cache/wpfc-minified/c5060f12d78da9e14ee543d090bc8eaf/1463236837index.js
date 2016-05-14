6ac0c308b0be63a9d4a0a5868b84903b016c8e38
Leonardo Rodriguez <leonrov@leonrov.local>
2016-05-14 12:52:15 -0300

    -.-

---
+++ wp-content/cache/wpfc-minified/c5060f12d78da9e14ee543d090bc8eaf/1463236837index.js
@@ -0,0 +1,38 @@
+// source --> http://cp.local.com/wp-content/themes/cp/js/scripts.js 
+$(document).ready(function(){
+  $("#share_fb").click(function(e){
+    e.preventDefault();
+    var data = $(this).data();
+    FB.ui({
+      method: 'feed',
+      link: data.link,
+      name: data.title,
+      caption: 'Carolina Parsonas',
+      description: data.excerpt,
+      picture: data.picture
+    }, function(response){});
+  });
+  $("#share_tw").click(function(e){
+    e.preventDefault();
+    var data = $(this).data();
+    var base = 'https://twitter.com/intent/tweet';
+    var text = encodeURIComponent( data.title );
+    var hashtags = '';
+    var via = '';
+    var url = encodeURIComponent( data.link );
+    var width  = 575,
+        height = 400,
+        urlTwit    = base + '?text=' + text + '&url=' + url + '&hashtags=' + hashtags ,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height;
+
+      window.open(urlTwit, 'twitter', opts);
+  });
+  $(".gallery-icon a").click(function(e){
+    e.preventDefault();
+    var photo = $(this).attr("href");
+    $("#photo_blog").attr("src", photo).show();
+    $("#myModal").modal('show')
+  });
+});
---
+++ wp-content/plugins/w3tc-wp-loader.php
@@ -0,0 +1,4 @@
+
+<?php
+    if (W3TC_WP_LOADING)
+        require_once '/Applications/MAMP/htdocs/cp/wp/wp-load.php';
