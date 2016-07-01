cfa991210b1eb3aa2afdba4da7cd0b6f3e4bc49e
Leonardo Rodriguez <leonrov@leonrov.local>
2016-06-30 21:23:19 -0300

    update

--- wp-content/themes/cp/templates/single-blog.php
+++ wp-content/themes/cp/templates/single-blog.php
@@ -1,9 +1,9 @@
 <!-- Content -->
 <section class="container section">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="deco2"></div>
-      <!--<?php $categories_blog = get_categories(array('parent' => 2, 'hide_empty' => 0)); ?>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="deco2"></div>
+            <!--<?php $categories_blog = get_categories(array('parent' => 2, 'hide_empty' => 0)); ?>
       <nav>
       <ul class="menu-blog">
         <?php if ( $categories_blog ) foreach ($categories_blog as $key => $cat) { ?>
@@ -12,71 +12,57 @@
       </ul>
       </nav>
       <div class="deco2"></div>-->
+        </div>
     </div>
-  </div>
-<div class="row">
-  <div class=" post-list col-md-9">
-    <?php if ( have_posts() ) : ?>
-
-    <?php /* Start the Loop */ ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-    <article class="post-content">
-      <header class="post-title">
-      <h2><?php the_title(); ?> </h2>
-      <h4><?php echo get_the_date(); ?></h4>
-      </header>
-       <?php if ( has_post_thumbnail() ) { ?>
-      <div class="post-img">
-        <?php $image = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>
-        <img src="<?php if ( has_post_thumbnail() ) echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>" alt="">
-      </div>
-      <?php } ?>
-
-      <div class="post-entry">
-      <?php the_content(); ?>
-      </div>
-
-      <div class ="blog_content readmore-social">
-
-      <ul class="socialbuttons share">
-        <li class="facebook">
-          <a  href=""
-          id="share_fb" 
-          data-link="<?php the_permalink(); ?>" 
-          data-title="<?php the_title(); ?>" 
-          data-excerpt="<?php echo strip_tags(get_the_excerpt()); ?>" 
-          data-picture="<?php echo $image; ?>" 
-          target="_blank">Facebook</a>
-        </li>
-        <li class="twitter">
-          <a  href=""
-          id="share_tw" 
-          data-link="<?php the_permalink(); ?>" 
-          data-title="<?php the_title(); ?>" 
-          target="_blank">twitter</a>
-        </li>
-      </ul>
-      </div>
-    </article>
-
     <div class="row">
-          <div class="deco2"></div>
-      <div class ="blog_content readmore-social">
-        <div class="pagination">
-          <?php
+        <div class=" post-list col-md-9">
+            <?php if ( have_posts() ) : ?>
+            <?php /* Start the Loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+            <article class="post-content">
+                <header class="post-title">
+                    <h2><?php the_title(); ?> </h2>
+                    <h4><?php echo get_the_date(); ?></h4>
+                </header>
+                <?php if ( has_post_thumbnail() ) { ?>
+                <div class="post-img">
+                    <?php $image = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>
+                    <img src="<?php if ( has_post_thumbnail() ) echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>" alt="">
+                </div>
+                <?php } ?>
+                <div class="post-entry">
+                    <?php the_content(); ?>
+                </div>
+                <div class="blog_content readmore-social">
+                    <ul class="socialbuttons share">
+                        <li class="facebook">
+                            <a href="" id="share_fb" data-link="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>" data-excerpt="<?php echo strip_tags(get_the_excerpt()); ?>" data-picture="<?php echo $image; ?>" target="_blank">Facebook</a>
+                        </li>
+                        <li class="twitter">
+                            <a href="" id="share_tw" data-link="<?php the_permalink(); ?>" data-title="<?php the_title(); ?>" target="_blank">twitter</a>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+            <div class="row">
+                <div class="deco2"></div>
+                <div class="blog_content readmore-social">
+                    <div class="pagination">
+                        <?php
           echo get_previous_post_link('%link', 'Anterior');
           echo get_next_post_link('%link', 'Siguiente');
           ?>
-        </div>
-      </div>
-    </div>
-
-
-    <div class="row" style="display: none">
-
-      <div class="deco2"></div>
-      <div class="blog_contentcomments">
-        <?php
+                    </div>
+                </div>
+            </div>
+            <div class="deco2"></div>
+            <div class="blog_contentcomments">
+                <?php echo do_shortcode('[fbcomments url="http://peadig.com/wordpress-plugins/facebook-comments/" width="375" count="off" num="3" countmsg="wonderful comments!"]'); ?>
+            </div>
+            <div class="row" style="display: none">
+                <div class="deco2"></div>
+                <div class="blog_contentcomments">
+                    <?php
         $fields =  array(
 
         'author' =>
@@ -123,51 +109,48 @@
         );
         comment_form($args);
         ?>
-      </div>
-
-      <?php endwhile; ?>
-      <?php else : ?>
-
-      <article id="post-0" class="post no-results not-found">
-        <header class="post-title">
-        <h1 ><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-        </header><!-- .entry-header -->
-
-        <div class="post-entry">
-          <p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-          <?php get_search_form(); ?>
-        </div><!-- .entry-content -->
-      </article><!-- #post-0 -->
-
-      <?php endif; ?>
-
-      <?php if ( get_comments() ) : ?>
-
-      <div class="post-content post-comments">    
-        <h3>Comentarios</h3>    
-        <ol class="comment-list">
-        <?php
+                </div>
+                <?php endwhile; ?>
+                <?php else : ?>
+                <article id="post-0" class="post no-results not-found">
+                    <header class="post-title">
+                        <h1><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+                    </header>
+                    <!-- .entry-header -->
+                    <div class="post-entry">
+                        <p>
+                            <?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?>
+                        </p>
+                        <?php get_search_form(); ?>
+                    </div>
+                    <!-- .entry-content -->
+                </article>
+                <!-- #post-0 -->
+                <?php endif; ?>
+                <?php if ( get_comments() ) : ?>
+                <div class="post-content post-comments">
+                    <h3>Comentarios</h3>
+                    <ol class="comment-list">
+                        <?php
         wp_list_comments( array(
         'style'       => 'ul',  
         ), get_comments() );
         ?>
-        </ol><!-- .comment-list -->
-        <?php endif; // have_comments() ?>
-        <?php
+                    </ol>
+                    <!-- .comment-list -->
+                    <?php endif; // have_comments() ?>
+                    <?php
         // If comments are closed and there are comments, let's leave a little note, shall we?
         if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
         ?>
-        <p class="no-comments">Comentarios cerrados</p>
-        <?php endif; ?>
-      </div>
+                        <p class="no-comments">Comentarios cerrados</p>
+                        <?php endif; ?>
+                </div>
+            </div>
+        </div>
+        <!--</div> CUANDO ENTRA UN COMENTARIO SE ROMPE!!! -->
+        <aside class="col-md-3">
+            <?php get_sidebar(); ?>
+        </aside>
     </div>
-  </div>
-  <!--</div> CUANDO ENTRA UN COMENTARIO SE ROMPE!!! --> 
-
-  <aside class="col-md-3">
-    <?php get_sidebar(); ?>
-  </aside>
-  </div>
-
 </section>
-
